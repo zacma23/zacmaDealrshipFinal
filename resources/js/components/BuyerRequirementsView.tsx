e8fc968ccabec9e468ba0b0fc9b97dbde7674a8c
zacma23 <zacma23@users.noreply.github.com>
2026-09-18 01:59:39 +0530

    Format trailing newlines in frontend components

--- resources/js/components/BuyerRequirementsView.tsx
+++ resources/js/components/BuyerRequirementsView.tsx
@@ -345,3 +345,4 @@ export const BuyerRequirementsView: React.FC<BuyerRequirementsViewProps> = ({
         </div>
     );
 };
+
--- resources/js/components/MessagesView.tsx
+++ resources/js/components/MessagesView.tsx
@@ -265,3 +265,4 @@ export const MessagesView: React.FC<MessagesViewProps> = ({
         </div>
     );
 };
+
